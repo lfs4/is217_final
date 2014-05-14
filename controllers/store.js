0c4e4ce014270db7eb1b89653b630f90bca1dd41
lou <lfs4@njit.edu>
2014-05-14 03:06:46 -0400

    added value input

--- controllers/store.js
+++ controllers/store.js
@@ -9,8 +9,10 @@ exports.makeOrder = function(req, res){
 	var screenSize = req.body['screenSize'];
 	var hardDriveSize = req.body['hardDriveSize'];
 	var memorySize = req.body['memorySize'];
-	var price = req.body['price'];
-	
+	var price = req.body['totalPrice'];
+
 	var laptop = new Laptop
+
+	
 	//decorator patterns used to set the latptop options
 }
