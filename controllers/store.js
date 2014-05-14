1cf12575d6912aeba532fe79a9ca647ae8845aba
lou <lfs4@njit.edu>
2014-05-14 02:47:48 -0400

    #1 working laptop page

---
+++ controllers/store.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'),
+Laptop = mongoose.model('Laptop');
+
+exports.laptop = function(req, res){
+	res.render('store', {title: 'Laptop Options'});
+}
+
+exports.makeOrder = function(req, res){
+	var screenSize = req.body['screenSize'];
+	var hardDriveSize = req.body['hardDriveSize'];
+	var memorySize = req.body['memorySize'];
+	var price = req.body['price'];
+	
+	var laptop = new Laptop
+	//decorator patterns used to set the latptop options
+}
